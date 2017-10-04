bd8c93a958c813abddaf89f82f37987bf112342e
Daniel Noon <supercookies@outlook.com>
2017-10-04 13:06:04 -0500

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Code.org Grapher
+Yesterday, in AP Computer Science Principles, I was supposed to draw a black-and-white picture in [code.org](https://code.org)'s b&w image widget. The format is a two-byte header (one byte representing the width and the other the height), and a stream of bits, each one representing a black or white pixel (0 for black, 1 for white).
+
+#### Example:
+```
+0000 0011
+0000 0101
+101010000010010
+```
+becomes
+!["A"](doc/a.png)
+
+## 
---
+++ test/test.js
@@ -0,0 +1,18 @@
+const Grapher = require("../lib/grapher");
+
+
+let parabola = new Grapher(254, 254, x => 2 * x * x + 3 * x);
+let line = new Grapher(254, 254, x => .5 * x, { axes: true });
+let sin = new Grapher(254, 254, x => 10 * Math.sin(x / 10), { x: "center", y: "center", axes: true });
+
+let multi = new Grapher(254, 254,
+  [
+    x => .25 * x * x,
+    x => 10 * Math.sqrt(x),
+    x => Math.abs(x),
+    x => 10 * Math.sin(Math.cos(x / 10))
+  ],
+  { axes: true, x: "center", y: "center" }
+);
+
+console.log(multi.graph());
